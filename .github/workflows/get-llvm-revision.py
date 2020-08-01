11497ecfd77264a8b9290a2d8dcbcbf15b85f662
David Maas <contact@pathogenstudios.com>
2020-08-01 09:43:51 -0500

    Updated LLVM revision test.

--- .github/workflows/get-llvm-revision.py
+++ .github/workflows/get-llvm-revision.py
@@ -5,8 +5,8 @@ import subprocess
 
 output = subprocess.check_output(["git", "submodule", "status", "external/llvm-project"], encoding='utf-8').strip()
 
-match = re.compile(r"^.(?P<sha1>[a-f0-9]+) external/llvm-project.*").match(output)
+match = re.match(r"^.(?P<revision>[a-f0-9]+) external/llvm-project.*", output)
 assert(match is not None), f"Malformed Git output: '{output}'"
 
-sha1 = match.group('sha1')
-print(f"::set-output name=sha1::{sha1}")
+revision = match.group('revision')
+print(f"::set-output name=revision::{revision}")
