7d5069c7a4dcb02e34bda75e834af39b33fe85ba
David Maas <contact@pathogenstudios.com>
2020-12-12 16:52:11 -0600

    Fixed error messages but better

--- .github/workflows/send-ci-failure-notification.py
+++ .github/workflows/send-ci-failure-notification.py
@@ -3,11 +3,13 @@ import os
 import sys
 from urllib import request
 
+import gha
+
 def get_environment_variable(name):
     ret = os.getenv(name)
 
     if ret is None or ret == '':
-        print(f"::error::Missing required parameter '{name}'", file=sys.stderr)
+        gha.print_error(f"Missing required parameter '{name}'")
 
     return ret
 
@@ -15,6 +17,7 @@ webhook_url = get_environment_variable('webhook_url')
 github_repo = get_environment_variable('github_repo')
 github_workflow_name = get_environment_variable('github_workflow_name')
 github_run_number = get_environment_variable('github_run_number')
+gha.fail_if_errors()
 
 card_data = f'''{{
   "type": "message",
