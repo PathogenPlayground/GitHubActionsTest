db0ecff53ec504d41f6c0c701114d0a83bc951a6
David Maas <contact@pathogenstudios.com>
2024-06-12 23:23:46 -0500

    fromJSON test

--- .github/workflows/fromjson-test.py
+++ .github/workflows/fromjson-test.py
@@ -3,4 +3,21 @@ import json
 
 import gha
 
-gha.set_output('matrix', '{"include":[{"project":"foo","config":"Debug"},\n{"project":"bar","config":"Release"}]}')
+matrix = {
+    'include': [
+        {
+            'project': 'foo',
+            'config': 'Debug'
+        },
+        {
+            'project': 'bar',
+            'config': 'Release'
+        },
+    ]
+}
+
+#gha.set_output('matrix', '{"include":[{"project":"foo","config":"Debug"},\n{"project":"bar","config":"Release"}]}')
+
+matrix_json = json.dumps({ "include": matrix }, indent=2)
+print(matrix_json)
+gha.set_output('matrix', matrix_json)
