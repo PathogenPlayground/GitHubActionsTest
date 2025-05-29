dc29fa9f0be610281c252e1d5bacd79252f59a14
David Maas <git@pixelalchemy.dev>
2025-05-29 14:10:12 -0500

    Warnings!

--- GitHubActionsTest/Test.cs
+++ GitHubActionsTest/Test.cs
@@ -2,7 +2,8 @@
 {
     public class Test
     {
-        public object Field;
+        public object Field; // Wowie
+        public object Field2;
 
         public override string ToString()
             => "Hello, world!";
